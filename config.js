a55fe574bb0eeda98092f11934f003e48656b8b6
JhoannS <sebastianzamudio2004@gmail.com>
2024-03-06 16:29:17 -0500

    actualizado

---
+++ config.js
@@ -0,0 +1,9 @@
+export const PORT = process.env.PORT || 4000
+export const DB_HOST = process.env.DB_HOST || 'Localhost'
+export const DB_USER= process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || ''
+export const DB_NAME = process.env.DB_NAME || 'Ae'
+export const DB_PORT = process.env.DB_PORT || '3307'
+
+
+
--- keys.js
+++ keys.js
@@ -1,10 +1,25 @@
 module.exports = {
     database: {
-        host: 'localhost',
+        host: 'monorail.proxy.rlwy.net',
         user: 'root',
-        password: '',
-        port:3307,
-        database: 'Ae'
+        password: 'Eegf4cE-Ch6ebG2A4gG6f2ABF1Ff1hfg',
+        port:51964,
+        database: 'railway'
         
     }
 }
+
+
+// import {DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER} from'./config';
+// import { createPool } from 'mysql2/promise';
+
+// export const pool = createPool({
+   
+    
+//         host: DB_HOST,
+//         user: DB_USER,
+//         password: DB_PASSWORD,
+//         port: DB_PORT,
+//         database: DB_NAME
+        
+// })
